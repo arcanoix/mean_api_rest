1ddd047afdb3a0a246cb3828464abc23d45a389f
thavoo <gustavoh.2312@gmail.com>
2018-01-30 22:35:41 -0400

    config setting view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("mean app");
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const indexRoute = require('./routes/index');
+const taskRoute = require('./routes/tasks');
+
+
+// settings
+app.set('views',path.join(__dirname, 'view'));
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use(indexRoute);
+app.use('/api',taskRoute);
+
+
+
+app.listen(app.get('port'), () => {
+    console.log('server on port ', app.get('port'));
+});
